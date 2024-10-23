0d6e0e7da28da9aebc4a149ff17009232e207116
Heloysa <heloysagama2@gmail.com>
2024-10-23 08:28:00 -0300

    deploy

---
+++ src/app/precos/page.tsx
@@ -0,0 +1,8 @@
+export default function Precos() {
+    return (
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-4">Preços</h1>
+        <p>Conteúdo da página de preços aqui.</p>
+      </div>
+    );
+  }
